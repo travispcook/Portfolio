c2562ed4aeb40cbcf4ae4c016bb4462865f4ce56
Travis Cook <travispcook@msn.com>
2018-04-16 13:42:02 -0600

    updated some stuffs

--- src/ticTacToe/index.style.js
+++ src/ticTacToe/index.style.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  .game-board {
-    margin: 10px auto;
+  &.game {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 10px;
+    .game-board {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      & > div {
+        display: flex;
+        flex-direction: column;
+      }
+    }
+
+    .game-info {
+      flex: 1;
+      flex-direction: column;
+      display: flex;
+      align-items: center;
+    }
   }
 `;
